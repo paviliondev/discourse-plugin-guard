a6b4a7e8106d0e145c5e1e0ace5bca495c34c91f
Angus McLeod <angus@mcleod.org.au>
2022-03-09 22:50:15 +0100

    Update files

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,6 +7,8 @@ ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_guard/extensions/discourse.rb ${DISCOURSE
 ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_guard/extensions/plugin_instance.rb ${DISCOURSE_ROOT}/lib/plugin_guard/extensions/plugin_instance.rb
 ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_guard/error.rb ${DISCOURSE_ROOT}/lib/plugin_guard/error.rb
 ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_guard/handler.rb ${DISCOURSE_ROOT}/lib/plugin_guard/handler.rb
+ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_guard/registration.rb ${DISCOURSE_ROOT}/lib/plugin_guard/registration.rb
+ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_guard/status.rb ${DISCOURSE_ROOT}/lib/plugin_guard/status.rb
 ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_guard/store.rb ${DISCOURSE_ROOT}/lib/plugin_guard/store.rb
 ln -sf ${PLUGIN_GUARD_ROOT}/lib/tasks/plugin_guard.rake ${DISCOURSE_ROOT}/lib/tasks/plugin_guard.rake
 ln -sf ${PLUGIN_GUARD_ROOT}/lib/plugin_initialization_guard.rb ${DISCOURSE_ROOT}/lib/plugin_initialization_guard.rb
